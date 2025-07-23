63897f4410118035c02aa25f9486d627eaa5f82c
moklad <dev.moklad@gmail.com>
2025-07-23 15:47:25 +0900

    feat: 뷰 디자인을 위한 프로토콜 추가

---
+++ NetworkProject/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  NetworkProject
+//
+//  Created by Youngjun Kim on 7/23/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol {
+    func configureHierarchy ()
+    func configureLayout()
+    func configureView()
+}
