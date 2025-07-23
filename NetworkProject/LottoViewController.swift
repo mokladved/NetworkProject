222753b182ecfcedd881a2dc06ca0d03287fa1ab
moklad <dev.moklad@gmail.com>
2025-07-23 18:47:20 +0900

    feat(LottoVC): 레이아웃 구현
    - infoLabel, dateLabel, lineView

--- NetworkProject/LottoViewController.swift
+++ NetworkProject/LottoViewController.swift
@@ -22,16 +22,22 @@ class LottoViewController: UIViewController {
     
     let infoLabel = {
         let label = UILabel()
+        label.text = "당첨번호 안내"
+        label.font = .boldSystemFont(ofSize: 15)
         return label
     }()
     
     let dateLabel = {
         let label = UILabel()
+        label.text = "2020-05-30 추첨"
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 13)
         return label
     }()
     
     let lineView = {
         let view = UIView()
+        view.backgroundColor = .lightGray
         return view
     }()
     
@@ -59,6 +65,29 @@ extension LottoViewController: ViewDesignProtocol {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
             make.top.equalTo(view.safeAreaLayoutGuide).offset(44)
             make.height.equalTo(50)
+        }
+        
+        labelWrappedView.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(15)
+            make.top.equalTo(textField.snp.bottom).offset(20)
+        }
+        
+        infoLabel.snp.makeConstraints { make in
+            make.leading.equalTo(labelWrappedView.snp.leading)
+            make.centerY.equalToSuperview()
+            make.top.bottom.equalToSuperview()
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(labelWrappedView.snp.trailing)
+            make.centerY.equalToSuperview()
+            make.top.bottom.equalToSuperview()
+        }
+        
+        lineView.snp.makeConstraints { make in
+            make.top.equalTo(labelWrappedView.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(labelWrappedView)
+            make.height.equalTo(1)
         }
     }
     
