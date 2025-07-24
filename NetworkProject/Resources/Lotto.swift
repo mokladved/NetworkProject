b606a0e6c857f7b3ae075263a270da82fb4d3fd6
moklad <dev.moklad@gmail.com>
2025-07-24 16:07:44 +0900

    feat(Model): Decodable 프로토콜을 채택한 구조체 생성
    - 회차, 당첨번호, 당첨발표 일자를 받을 수 있는 구조체

---
+++ NetworkProject/Resources/Lotto.swift
@@ -0,0 +1,20 @@
+//
+//  Lotto.swift
+//  NetworkProject
+//
+//  Created by Youngjun Kim on 7/24/25.
+//
+
+import Foundation
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let drwNo: Int
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
